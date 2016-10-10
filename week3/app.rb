6348f5458d851206be1cc3e1baea23077eb459d9
cgulli <caitlin.gulliford@gmail.com>
2016-10-10 15:16:20 +0100

    step 8: get an octopus

--- week3/app.rb
+++ week3/app.rb
@@ -11,3 +11,10 @@ end
 get '/luke' do
   "Go forward to go back."
 end
+
+get '/octopus' do
+  "<div style='border: 3px dashed red'>
+  <img src= 'http://i.giphy.com/118pNUM9rV3HRC.gif'>
+  </div>"
+
+end
